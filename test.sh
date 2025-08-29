bc416944b44a0e77bfdcc649c2de65a1157becc4
Frédéric Bour <frederic.bour@lakaban.net>
2025-08-29 17:49:53 +0900

    Downgrade grammar to OCaml 5.3.0
    Otherwise there are too many errors.

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ dune exec bin/main.exe -- \
 --avoid METAOCAML_ESCAPE \
 --avoid METAOCAML_BRACKET_OPEN \
 --avoid METAOCAML_BRACKET_CLOSE \
+--avoid 'let_binding_body: val_ident' \
 --weight '1000 list(attribute):' \
 --weight '1000 list(post_item_attribute):' \
 --weight '0.01 floating_attribute' \
